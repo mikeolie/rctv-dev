5b267e8c43e75516fa5af520204e17b02a096591
Michael Olie <molie@rarecollectiblestv.com>
2022-04-05 15:38:32 -0700

    added code for block

---
+++ app/code/Rctv/BannerBlock/Block/Banner.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Rctv\BannerBlock\Block;
+
+class Block extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+}
---
+++ app/code/Rctv/BannerBlock/registration.php
@@ -0,0 +1,6 @@
+<?php
+Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Rctv_BannerBlock',
+    __DIR__
+);
